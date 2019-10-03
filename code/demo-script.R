d0778e73be44be4b9b0b79d9fa21a4c7c88e7d4a
Ellese <ellesenguyen@gmail.com>
2019-10-03 15:47:38 -0700

    add: demo script

---
+++ code/demo-script.R
@@ -0,0 +1,11 @@
+# ===================================================================
+# Title: Demo Script for R
+# Description: This is just a demo script for Lab05 in STAT133
+# Inputs: NA
+# Outputs: NA
+# Author: Ellese Nguyen
+# Date: 10-3-2019
+# ===================================================================
+
+library(dplyr)
+library(ggplot2)
